b8d480f51fff74d004f545d8545e099dd773c9b9
cdevjarrell <christian.j.jarrell@gmail.com>
2021-03-16 21:57:43 -0400

    Create post and delete route.

--- routes/videoLibrary.js
+++ routes/videoLibrary.js
@@ -2,13 +2,40 @@ const express = require('express');
 const router = express.Router();
 const Video = require("../schema/videoSchema");
 const Joi = require('joi');
+const Comment = require("../schema/commentSchema")
 
 router.get("/", async (req, res) => {
     try {
         const videos = await Video.find();
         return res.send(videos);
     } catch(error) {
-        return res.status(400).send(`Database error ${error}`);
+        return res.status(400).send(`Database Error ${error}`);
+    }
+});
+
+router.post('/', async (req, res) => {
+    let videos = new Video({
+        videoTitle: req.body.videoTitle,
+        likes: req.body.likes,
+        dislikes: req.body.dislikes,
+    });
+    try{
+        const result = await videos.save();
+        return res.send(result);
+    } catch (error) {
+        return res.status(400).send(`Database Error ${error}`);
+    }
+});
+
+router.delete("/:id", async (req, res) => {
+    try {
+        const videos = await Video.findByIdAndRemove(req.params.id);
+
+        if (!videos) return res.status(400).send(`The video with id "${req.params.id} does not exist.`);
+
+        return res.send(videos);
+    } catch (ex) {
+        return res.status(500).send(`Database Error: ${ex}`);
     }
 });
 
